f1f146c7b95086707be17b5c3920d33b5eee72d6
Nakay23 <smaslov43@mail.ru>
2024-10-01 18:29:31 +0300

    Исправил задание 1

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@ public class Main {
         System.out.println("Hello world!");
 
         int clientOS = 0;
-        int clientDeviceYear = 2015;
+        int clientDeviceYear = 2016;
         if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-            } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            }
+        if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            }
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-            } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            }
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
 
